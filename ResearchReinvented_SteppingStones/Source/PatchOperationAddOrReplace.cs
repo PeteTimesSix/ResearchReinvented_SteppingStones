52fb1efda185834f6e1992cdfd22bbf96207b649
PeteTimesSix <Pete1001@seznam.cz>
2022-10-21 13:26:06 +0200

    v1.4
    also removed xml extensions dependency

---
+++ ResearchReinvented_SteppingStones/Source/PatchOperationAddOrReplace.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Verse;
+
+namespace RR
+{
+    //code taken with permission from https://github.com/15adhami/XmlExtensions/blob/main/Source/PatchOperations/PatchOperations/PatchOperationAddOrReplace.cs
+
+    public class PatchOperationAddOrReplace : PatchOperationPathed
+    {
+        public XmlContainer value;
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            XmlNode node = value.node;
+            XmlNode foundNode = null;
+            bool matched = false;
+            foreach (XmlNode xmlNode in xml.SelectNodes(xpath).Cast<XmlNode>().ToArray())
+            {
+                matched = true;
+                foreach (XmlNode addNode in node.ChildNodes)
+                {
+                    if (!ContainsNode(xmlNode, addNode, ref foundNode))
+                    {
+                        xmlNode.AppendChild(xmlNode.OwnerDocument.ImportNode(addNode, true));
+                    }
+                    else
+                    {
+                        xmlNode.InsertAfter(xmlNode.OwnerDocument.ImportNode(addNode, true), foundNode);
+                        xmlNode.RemoveChild(foundNode);
+                    }
+                }
+            }
+            return matched;
+        }
+
+        protected bool ContainsNode(XmlNode parent, XmlNode node)
+        {
+            XmlNode temp = null;
+            return ContainsNode(parent, node, ref temp);
+        }
+
+        protected bool ContainsNode(XmlNode parent, XmlNode node, ref XmlNode foundNode)
+        {
+            XmlAttributeCollection attrs = node.Attributes;
+            foreach (XmlNode childNode in parent.ChildNodes)
+            {
+                if (childNode.Name == node.Name)
+                {
+                    XmlAttributeCollection attrsChild = childNode.Attributes;
+                    if (attrs == null && attrsChild == null)
+                    {
+                        foundNode = childNode;
+                        return true;
+                    }
+                    if (attrs != null && attrsChild != null && attrs.Count == attrsChild.Count)
+                    {
+                        bool b = true;
+                        foreach (XmlAttribute attr in attrs)
+                        {
+                            XmlNode attrChild = attrsChild.GetNamedItem(attr.Name);
+                            if (attrChild == null)
+                            {
+                                b = false;
+                                break;
+                            }
+                            if (attrChild.Value != attr.Value)
+                            {
+                                b = false;
+                                break;
+                            }
+                        }
+                        if (b)
+                        {
+                            foundNode = childNode;
+                            return true;
+                        }
+                    }
+                }
+            }
+            foundNode = null;
+            return false;
+        }
+    }
+}
